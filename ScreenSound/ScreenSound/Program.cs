5e0d888fca15c12bba9ec0b609c9ca2ecaaa9890
xZuckert <xXPZCOCAXx@outlook.com>
2025-08-20 14:47:06 -0300

    Adição da função de adição de banda

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,22 +1,32 @@
-﻿void ExibirMensagem(string logo, string msg){
-    Console.WriteLine(logo);
+﻿Console.Clear(); string logo = @"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+"; string msgBoasVindas = "Boas Vindas ao Screen Sound";
+void ExibirMensagem(string logo, string msg){
+    Console.WriteLine(logo); 
+    Thread.Sleep(500);
     Console.WriteLine("***************************");
     Console.WriteLine(msg);
     Console.WriteLine("***************************");
+    Thread.Sleep(500);
 }
-
 void ExibirOpçõesMenu() {
+    ExibirMensagem(logo, msgBoasVindas);
     Console.WriteLine("1 - Registrar uma banda");
     Console.WriteLine("2 - Mostrar Bandas");
     Console.WriteLine("3 - Avaliar uma banda");
     Console.WriteLine("4 - Exibir a média de uma banda");
     Console.WriteLine("0 - Sair");
-    
     Console.Write("\nDigite sua opção: ");
     string opcaoEscolhida = Console.ReadLine()!;
     int opcaoEscolhidaInt = int.Parse(opcaoEscolhida);
+    Thread.Sleep(700);
     switch (opcaoEscolhidaInt) {
-        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaInt); break;
+        case 1: RegistrarBanda(); break;
         case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaInt); break;
         case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaInt); break;
         case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaInt); break;
@@ -25,15 +35,16 @@ void ExibirOpçõesMenu() {
     }
 }
 
-string logo = @"
-░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
-╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
-░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
-██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
-";
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de Banda: ");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(1000);
+    Console.Clear();
+    ExibirOpçõesMenu();
+}
 
-string msgBoasVindas = "Boas Vindas ao Screen Sound";
-ExibirMensagem(logo, msgBoasVindas);
 ExibirOpçõesMenu();
