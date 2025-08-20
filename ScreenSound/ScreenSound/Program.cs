4dbcdb213475b663d7f5935c26112b23bc887153
xZuckert <xXPZCOCAXx@outlook.com>
2025-08-20 13:10:30 -0300

    Adição das opções do menu com ReadLine

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -11,6 +11,18 @@ void ExibirOpçõesMenu() {
     Console.WriteLine("3 - Avaliar uma banda");
     Console.WriteLine("4 - Exibir a média de uma banda");
     Console.WriteLine("0 - Sair");
+    
+    Console.Write("\nDigite sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaInt = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaInt) {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaInt); break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaInt); break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaInt); break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaInt); break;
+        case 0: Console.WriteLine("Programa encerrado!"); break;
+        default: Console.WriteLine("Opção Inválida"); break;
+    }
 }
 
 string logo = @"
--- ScreenSound/ScreenSound/obj/Debug/net9.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/ScreenSound/obj/Debug/net9.0/ScreenSound.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80daf083182c294962ed19b133e8be798baa037f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
