908a2a9f320bfea9128fced98fdc887f0879a197
xZuckert <xXPZCOCAXx@outlook.com>
2025-08-20 12:47:02 -0300

    Adição do menu

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,2 +1,27 @@
-﻿string msgBoasVindas = "Boas vindas ao Screen Sound";
-Console.WriteLine(msgBoasVindas);
+﻿void ExibirMensagem(string logo, string msg){
+    Console.WriteLine(logo);
+    Console.WriteLine("***************************");
+    Console.WriteLine(msg);
+    Console.WriteLine("***************************");
+}
+
+void ExibirOpçõesMenu() {
+    Console.WriteLine("1 - Registrar uma banda");
+    Console.WriteLine("2 - Mostrar Bandas");
+    Console.WriteLine("3 - Avaliar uma banda");
+    Console.WriteLine("4 - Exibir a média de uma banda");
+    Console.WriteLine("0 - Sair");
+}
+
+string logo = @"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+";
+
+string msgBoasVindas = "Boas Vindas ao Screen Sound";
+ExibirMensagem(logo, msgBoasVindas);
+ExibirOpçõesMenu();
